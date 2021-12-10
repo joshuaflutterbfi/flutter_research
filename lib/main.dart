f58af76f82b347b0573cc9d53ed721a98eb0c8fb
Bimo Satriantoro <bimo.satriantoro@bfi.co.id>
2021-12-10 10:23:18 +0700

    Change Log :
    Speech to text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_research/home/screen/home_screen.dart';
+import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'router.dart';
@@ -7,6 +7,9 @@ import 'router.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarBrightness: Brightness.light,
+  ));
   runApp(MyApp());
 }
 
